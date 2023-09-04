ab17171cfde231d39af6c6f88d2b0cc5c3a9b918
Anas Boustani <sino3791@gmail.com>
2023-09-05 01:36:18 +0200

    fixed filenames

---
+++ shserial/include/shserial/shSerial.h
@@ -0,0 +1,132 @@
+#ifndef SH_SERIAL_H
+#define SH_SERIAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+#ifdef _WIN32
+
+#include <windows.h>
+
+#else
+
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+
+#endif//_WIN32
+
+
+#include <stdint.h>
+
+
+
+#ifdef _WIN32
+	#define shSerialBadResult(result) (result < 1)
+#else
+	#define shSerialBadResult(result) (result < 0)
+#endif//_WIN32
+
+
+#define shSerialError(condition, error_msg, failure_expression)\
+	if ((int)(condition)) {\
+		printf("shserial error: %s, %s.\n", error_msg, shSerialGetError()); failure_expression;\
+	}
+
+
+
+typedef enum ShSerialFlags {
+#ifdef _WIN32
+
+	SH_SERIAL_READ       = GENERIC_READ,
+	SH_SERIAL_WRITE      = GENERIC_WRITE,
+	SH_SERIAL_READ_WRITE = GENERIC_READ | GENERIC_WRITE,
+
+#else
+  
+	SH_SERIAL_READ       = O_RDONLY,
+	SH_SERIAL_WRITE      = O_WRONLY,
+	SH_SERIAL_READ_WRITE = O_RDWR,
+
+#endif//_WIN32
+} ShSerialFlags;
+
+
+
+#ifdef _WIN32
+
+typedef HANDLE ShSerialHandle;
+#define SH_SERIAL_NULL_HANDLE NULL
+
+#else
+
+typedef int ShSerialHandle
+#define SH_SERIAL_NULL_HANDLE 0
+
+#endif//_WIN32
+
+
+
+#ifdef _WIN32
+
+typedef enum ShSerialCommMask {
+	SH_SERIAL_EV_BREAK     = EV_BREAK,		
+	SH_SERIAL_EV_CTS       = EV_CTS,			
+	SH_SERIAL_EV_DSR       = EV_DSR,			
+	SH_SERIAL_EV_ERR       = EV_ERR,			
+	SH_SERIAL_EV_EVENT1    = EV_EVENT1,		
+	SH_SERIAL_EV_EVENT2    = EV_EVENT2,		
+	SH_SERIAL_EV_PERR      = EV_PERR, 		
+	SH_SERIAL_EV_RING      = EV_RING, 		
+	SH_SERIAL_EV_RLSD      = EV_RLSD, 		
+	SH_SERIAL_EV_RX80FULL  = EV_RX80FULL,	
+	SH_SERIAL_EV_RXCHAR    = EV_RXCHAR,		
+	SH_SERIAL_EV_RXFLAG    = EV_RXFLAG,		
+	SH_SERIAL_EV_TXEMPTY   = EV_TXEMPTY,	
+} ShSerialCommMask;
+
+#endif//_WIN32
+
+
+
+extern void shSerialSleep(
+	uint32_t ms
+);
+
+extern uint8_t shSerialOpen(
+	char*           port, 
+	uint32_t        baud_rate, 
+	uint32_t        read_timeout_ms, 
+	ShSerialFlags   flags, 
+	ShSerialHandle* p_handle
+);
+
+extern uint8_t shSerialWrite(
+	uint32_t        size, 
+	void*           src, 
+	uint32_t*       p_bytes_written,
+	ShSerialHandle  handle
+);
+
+extern uint8_t shSerialRead(
+	uint32_t        size, 
+	void*           dst,
+	uint32_t*       p_bytes_read,
+	ShSerialHandle  handle
+);
+
+extern uint8_t shSerialClose(
+	ShSerialHandle handle
+);
+
+extern char* shSerialGetError();
+
+
+
+
+#ifdef __cplusplus
+}
+#endif//__cplusplus
+
+#endif//SH_SERIAL_H
---
+++ shserial/src/shserial/shSerial.c
@@ -0,0 +1,387 @@
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+#include "shserial/shSerial.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef _WIN32
+#include <termios.h>
+#include <errno.h>
+#endif // _WIN32
+
+#ifdef _MSC_VER
+#pragma warning (disable: 28193 4996)
+#endif // _MSC_VER
+
+
+
+void shSerialSleep(
+	uint32_t ms
+) {
+#ifdef _WIN32
+	Sleep(ms);
+#else
+	usleep(ms*1000);
+#endif
+}
+
+uint8_t shSerialOpen(
+	char*           port, 
+	uint32_t        baud_rate, 
+	uint32_t        read_timeout_ms, 
+	ShSerialFlags   flags, 
+	ShSerialHandle* p_handle
+) {
+	shSerialError(
+		p_handle == NULL,
+		"shSerialOpen: invalid handle memory",
+		return 0
+	);
+
+	shSerialError(
+		port == NULL,
+		"shSerialOpen: invalid port memory",
+		return 0
+	);
+
+
+
+#ifdef _WIN32
+
+	BOOL r         = 0;
+	char _port[32] = "\\\\.\\";
+	
+	strcat(_port, port);
+
+	(*p_handle) = CreateFile(
+		_port,                 //lpFileName
+		flags,                 //dwDesiredAccess
+		0,                     //dwShareMode
+		NULL,                  //lpSecurityAttributes
+		OPEN_EXISTING,         //dwCreationDisposition
+		0,                     //dwFlagsAndAttributes
+		NULL                   //hTemplateFile
+	);
+	
+	shSerialError(
+		(*p_handle) == INVALID_HANDLE_VALUE,
+		"shSerialOpen: cannot open serial port",
+		return 0
+	);
+
+	DCB src_dcb = { 0 };
+
+	r = GetCommState(
+		(*p_handle), //hFile
+		&src_dcb     //lpDCB
+	);
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: failed getting current device control block",
+		return 0
+	);
+
+	src_dcb.BaudRate = baud_rate;
+	src_dcb.StopBits = ONESTOPBIT;
+	src_dcb.Parity   = NOPARITY;
+	src_dcb.ByteSize = 8;
+
+	r = SetCommState(
+		(*p_handle), //hFile
+		&src_dcb     //lpDCB
+	);
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: failed setting device control block",
+		return 0
+	);
+
+	COMMTIMEOUTS timeout = {
+		read_timeout_ms, //ReadIntervalTimeout
+		1,               //ReadTotalTimeoutMultiplier
+		1,               //ReadTotalTimeoutConstant
+		1,               //WriteTotalTimeoutMultiplier
+		1                //WriteTotalTimeoutConstant
+	};
+	
+	r = SetCommTimeouts(
+		(*p_handle), //hFile
+		&timeout     //lpCommTimeouts
+	);
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: cannot set timeouts",
+		return 0
+	);
+
+#else
+
+	int r = 0;
+
+	(*p_handle) = open(
+		port,            //pathname
+		flags | O_NOCTTY //flags
+	);
+
+	shSerialError(
+		(*p_handle) == INVALID_HANDLE_VALUE,
+		"shSerialOpen: cannot open serial port",
+		return 0
+	);
+
+	struct termios dcb = { 0 };
+
+	r = tcgetattr(
+		(*p_handle), //fd 
+		&dcb         //termios_p
+	);
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: failed getting current device control block",
+		return 0
+	);
+
+	r = cfsetispeed(
+		&dcb,     //termios_p
+		baud_rate //speed
+	);
+	
+	r += cfsetospeed(
+		&dcb,     //termios_p
+		baud_rate //speed
+	);
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: failed setting baud rate",
+		return 0
+	);
+
+	dsb.c_cflag     &=  ~PARENB;            // Make 8n1
+	dsb.c_cflag     &=  ~CSTOPB;
+	dsb.c_cflag     &=  ~CSIZE;
+	dsb.c_cflag     |=  CS8;
+	dsb.c_cflag     &=  ~CRTSCTS;           // no flow control
+	dsb.c_cc[VMIN]   =  1;                  // read doesn't block
+	dsb.c_cc[VTIME]  =  (int)((float)read_timeout_ms / 100.0f); // 0.5 seconds read timeout
+	dsb.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+	
+	r = cfmakeraw(
+		&dcb //termios_p
+	);
+	
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: failed setting device control block",
+		return 0
+	);
+
+	r = tcflush(
+		(*p_handle), //fd
+		TCIFLUSH     //queue_selector
+	);
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: failed setting queue selector",
+		return 0
+	);
+	
+	r = tcsetattr(
+		(*p_handle), //fd
+		TCSANOW,     //optional_actions
+		&dcb         //termios_p
+	);
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialOpen: failed setting device control block",
+		return 0
+	);
+
+#endif//WIN32
+
+	return 1;
+}
+
+uint8_t shSerialClose(
+	ShSerialHandle handle
+) {
+	shSerialError(
+		handle == SH_SERIAL_NULL_HANDLE,
+		"shSerialRead: invalid handle memory",
+		return 0
+	);
+
+
+#ifdef _WIN32
+
+	BOOL r = 0;
+
+	r = CloseHandle(handle);
+
+#else
+
+	int r = 0;
+
+	r = close(handle);
+
+#endif//_WIN32
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialRead: failed closing serial port",
+		return 0
+	);
+
+	return 1;
+}
+
+uint8_t shSerialRead(
+	uint32_t       size, 
+	void*          dst,
+	uint32_t*      p_bytes_read,
+	ShSerialHandle handle
+) {
+	shSerialError(
+		handle == SH_SERIAL_NULL_HANDLE,
+		"shSerialRead: invalid handle memory",
+		return 0
+	);
+
+	shSerialError(
+		size == 0,
+		"shSerialRead: invalid read size",
+		return 0
+	)
+
+	shSerialError(
+		dst == NULL,
+		"shSerialRead: invalid dst memory",
+		return 0
+	)
+
+#ifdef _WIN32
+
+	BOOL  r          = 0;
+	DWORD bytes_read = 0;
+
+	r = ReadFile(
+		handle,      //hFile
+		dst,         //lpBuffer
+		size,        //nNumberOfBytesToRead
+		&bytes_read, //lpNumberOfBytesRead
+		NULL         //lpOverlapped
+	);
+
+#else
+
+	uint8_t  r          = 1;
+	uint32_t bytes_read = 0;
+
+	bytes_read = (uint32_t)read(
+		p_handle, //
+		dst,      //
+		size      //
+	);
+
+#endif//_WIN32
+
+	if (p_bytes_read != NULL) {
+		(*p_bytes_read) = (uint32_t)bytes_read;
+	}
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialRead: failed reading from serial port",
+		return 0
+	);
+
+	return 1;
+}
+
+uint8_t shSerialWrite(
+	uint32_t        size, 
+	void*           src, 
+	uint32_t*       p_bytes_written,
+	ShSerialHandle* handle
+) {
+	shSerialError(
+		handle == SH_SERIAL_NULL_HANDLE,
+		"shSerialWrite: invalid handle memory",
+		return 0
+	);
+
+	shSerialError(
+		size == 0,
+		"shSerialWrite: invalid read size",
+		return 0
+	)
+
+	shSerialError(
+		src == NULL,
+		"shSerialWrite: invalid dst memory",
+		return 0
+	)
+
+
+#ifdef _WIN32
+
+	BOOL  r             = 0;
+	DWORD bytes_written = 0;
+
+	bytes_written = WriteFile(
+		handle,         //hFile
+		src,            //lpBuffer
+		size,           //nNumberOfBytesToWrite
+		&bytes_written, //lpNumberOfBytesWritten
+		NULL            //lpOverlapped
+	);
+
+#else
+
+	uint8_t  r             = 1;
+	uint32_t bytes_written = 0;
+
+	bytes_written = (uint32_t)write(
+		p_handle, //
+		src,      //
+		size      //
+	);
+
+#endif // _WIN32
+
+	if (p_bytes_written != 0) {
+		(*p_bytes_written) = (uint32_t)bytes_written;
+	}
+
+	shSerialError(
+		shSerialBadResult(r),
+		"shSerialWrite: failed writing to serial port",
+		return 0
+	);
+
+	return 1;
+}
+
+char* shSerialGetError() {
+#ifdef _WIN32
+	return "unknown error";
+#else
+	return strerror(errno);
+#endif // _WIN32
+}
+
+
+
+#ifdef __cplusplus
+}
+#endif//__cplusplus
